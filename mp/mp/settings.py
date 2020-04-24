b7ce32036054dcaf2043e3d2866b7f75b4507103
Anoop Christopher Thiparala <30379425+anpct@users.noreply.github.com>
2020-04-25 02:05:37 +0530

    Update settings.py

--- mp/mp/settings.py
+++ mp/mp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&n=d7om8-7n2yz%(-(p$&hq3hf530a_-^=t=$hn3(bcm)e52*%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['13.127.165.186']
+ALLOWED_HOSTS = ['13.127.165.186', 'anonimage.ml']
 
 
 # Application definition
