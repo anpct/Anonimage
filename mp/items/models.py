13188e70dd57b8cced66bbb88affef77625a6105
Anoop Christopher Thiparala <30379425+anpct@users.noreply.github.com>
2020-04-26 02:55:04 +0530

    Update models.py

--- mp/items/models.py
+++ mp/items/models.py
@@ -10,7 +10,7 @@ import sys
 from django.core.exceptions import ValidationError
 
 def file_size(value): # add this to some file where you can import it from
-    limit = 500 *1024
+    limit = 2*1024 *1024
     if value.size > limit:
         raise ValidationError('File too large. Size should not exceed 500KB.')
     
