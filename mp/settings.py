1f3b6e0afc61725561e979bbd7dc11f5016ca165
Anoop Christopher Thiparala <30379425+anpct@users.noreply.github.com>
2020-05-25 12:56:32 +0530

    Update settings.py

--- mp/settings.py
+++ mp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['anonimage.ml', 'www.anonimage.ml']
+ALLOWED_HOSTS = ['anonimage.ml', 'www.anonimage.ml', 'anonimage.herokuapp.com', 'www.anonimage.herokuapp.com']
 
 
 # Application definition
