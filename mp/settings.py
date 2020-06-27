7267637a4027afacb25a1786c523ee657841a114
Anoop Christopher Thiparala <30379425+anpct@users.noreply.github.com>
2020-06-27 12:06:42 +0530

    minor

--- mp/settings.py
+++ mp/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     
 }
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
