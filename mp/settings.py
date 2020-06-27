56ecf0a4bdfe8b18336d6e3e538a02a10e100b33
Anoop Christopher Thiparala <30379425+anpct@users.noreply.github.com>
2020-06-27 19:19:31 +0530

    Update settings.py

--- mp/settings.py
+++ mp/settings.py
@@ -135,7 +135,7 @@ if USE_S3:
     AWS_ACCESS_KEY_ID = os.getenv('FELIX_AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('FELIX_AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('FELIX_BUCKET_NAME')
-    AWS_DEFAULT_ACL = "public-read-write"
+    AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.ap-south-1.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 public media settings
